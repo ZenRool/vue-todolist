14ba841bd3be79a5b5f22028d0b347867ef11c6b
Zenrool <lorenzopapperini@hotmail.it>
2022-05-09 17:10:58 +0200

    Struttura html e css, classe todo in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-const app = new Vue( {} );
+const app = new Vue( {
+    // div dom di Vue 
+    el: "#root",
+    data: ( {
+        todo:
+            [
+                new Todo("Fai il millestone 1", false),
+                new Todo("Fai il millestone 2", false),
+                new Todo("Fai il millestone 3", false),
+                new Todo("Fai il bonus 1", false),
+                new Todo("Fai il bonus 2", false),
+                new Todo("Porta fuori il cane", false),
+                new Todo("Fai la spesa", false),
+                new Todo("Prepara da mangiare", false),
+                new Todo("Compra il CAFFÈ", false),
+            ]
+    
+    }),
+    methods: {
+    }
+} 
+);
+
+class Todo {
+    
+    constructor( text , done ) {
+        this.text = text;
+        this.done = done;
+    }
+}
